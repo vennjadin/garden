4bad3a1c2d8f3919b2e93614fd8a50990a9a1b5c
Benjamin Gaupset <benjagau@gmail.com>
2021-12-08 14:34:26 +0100

    added schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -8,10 +8,35 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // Hello world
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: 'mySchema',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      // This is the display name for the type
+      title: "Person",
+      
+      // The identifier for this document type used in the api's
+      name: "person",
+      
+      // Documents have the type 'document'. Your schema may describe types beyond documents
+      // but let's get back to that later.
+      type: "document",
+      
+      // Now we proceed to list the fields of our document
+      fields: [
+        // This document has only one field
+        {
+          // The display name for this field
+          title: "Name",
+
+          // The identifier for this field used in the api's
+          name: "name",
+
+          // The type of this field
+          type: "string",
+        }
+      ]
+    }
   ]),
 })
